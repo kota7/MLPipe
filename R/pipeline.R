b3623793722bd422f5c1173561f787a12c72010d
kota <kmori05@gmail.com>
2017-04-30 16:42:08 +0900

    pipeline example

--- R/pipeline.R
+++ R/pipeline.R
@@ -1,66 +1,76 @@
-#' Machine learning pipeline
-#'
-#' @param ... Arbitrary number of pipeline components
-#' @examples
-#' library(mlbench)
-#' data(Sonar)
-#' X <- Sonar[, -ncol(Sonar)]
-#' y <- Sonar[, ncol(Sonar)]
-#' samp <- c(sample(1:111,75), sample(112:200,75))
-#'
-#' p <- pipeline(pc=pca(30), ml=mlp_classifier(size=10))
-#' p$fit((list(x=X[samp,], y=y[samp])))
-#' table(y[-samp], p$predict(list(x=X[-samp,])))
-#' @export
-pipeline <- function(...) { Pipeline$new(...) }
 
 
 Pipeline <- R6::R6Class(
-  'Pipeline',
-  inherit=PipeComponent,
+  'Pipeline', inherit=PipeComponent,
 
   public = list(
     steps=list(),
 
-    fit = function(data)
+    fit = function(x, y=NULL)
     {
       for (k in seq_along(self$steps))
       {
-        self$steps[[k]]$fit(data)
-        data <- self$steps[[k]]$transform(data)
-      }
-    },
-
-    transform = function(data)
-    {
-      for (k in seq_along(self$steps))
-      {
-        data <- self$steps[[k]]$transform(data)
-      }
-      data
-    },
-
-    predict = function(data)
-    {
-      for (k in seq_along(self$steps))
-      {
-        if (k != length(self$steps)) {
-          data <- self$steps[[k]]$transform(data)
-        } else {
-          return(self$steps[[k]]$predict(data))
+        self$steps[[k]]$fit(x, y)
+        if (k < length(self$steps)) {
+          tmp <- self$steps[[k]]$transform(x, y)
+          x <- tmp$x
+          y <- tmp$y
         }
       }
     },
 
-    incfit = function(data)
+    transform = function(x, y=NULL)
     {
+      dat <- list(x=x, y=y)
       for (k in seq_along(self$steps))
       {
-        self$steps[[k]]$incfit(data)
-        data <- self$steps[[k]]$transform(data)
+        dat <- self$steps[[k]]$transform(dat$x, dat$y)
+      }
+      dat
+    },
+
+    predict = function(x)
+    {
+      dat <- list(x=x, y=y)
+      for (k in seq_along(self$steps))
+      {
+        if (k != length(self$steps)) {
+          dat <- self$steps[[k]]$transform(dat$x, dat$y)
+        } else {
+          return(self$steps[[k]]$predict(dat$x))
+        }
+      }
+    },
+
+    incfit = function(x, y=NULL)
+    {
+      dat <- list(x=x, y=y)
+      for (k in seq_along(self$steps))
+      {
+        self$steps[[k]]$incfit(dat$x, dat$y)
+        dat <- self$steps[[k]]$transform(dat$x, dat$y)
       }
     },
 
     initialize = function(...) { self$steps <- list(...) }
   )
 )
+
+
+
+#' Machine learning pipeline
+#'
+#' @param ... Arbitrary number of pipeline components
+#' @examples
+#' set.seed(123)
+#' data(Sonar, package='mlbench')
+#' X <- Sonar[, -ncol(Sonar)]
+#' y <- Sonar[, ncol(Sonar)]
+#' tr <- c(sample(1:111,75), sample(112:200,75))
+#'
+#' p <- pipeline(pc=pca_extractor(30),
+#'               ml=mlp_classifier(hidden_sizes=c(5, 5), num_epoch=1000))
+#' p$fit(X[tr,], y[tr])
+#' table(y[-tr], p$predict(X[-tr,]))
+#' @export
+pipeline <- Pipeline$new
